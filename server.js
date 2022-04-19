f1b30e4cc767498003ce04c1962c14d906582f2f
meghanav27 <meghanav27@gmail.com>
2022-04-19 11:43:18 -0400

    server

--- server.js
+++ server.js
@@ -5,4 +5,11 @@ import { coinFlips, countFlips, coinFlip, flipACoin } from "./modules/coin.mjs";
 
 const express = require('express')
 const app = express()
-const arg = minimist(process.argv.slice(2))
+
+const arg = require('minimist')(process.argv.slice(2))
+args['port']
+const port = args.port || process.env.PORT || 5000
+
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
+});
