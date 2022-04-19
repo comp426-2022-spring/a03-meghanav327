2274c303b306ee90c5b2400c14c9171c08471356
meghanav27 <meghanav27@gmail.com>
2022-04-19 11:53:52 -0400

    first flip

--- server.js
+++ server.js
@@ -13,3 +13,20 @@ const port = args.port || process.env.PORT || 5000
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
+
+app.get('/app/', (req, res) => {
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
+    res.end(res.statusCode + ' ' + res.statusMessage)
+});
+
+app.get('/app/flip', (req, res) => {
+    res.statusCode = 200;
+    res.type('text/plain')
+    res.json({ 'flip': coinFlip() })
+});
+
+app.use(function(req, res) {
+    res.status(404).send('404 NOT FOUND')
+});
